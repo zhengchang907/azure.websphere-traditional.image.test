e960e6005fc7a76e195c52da7f9085d085904b33
Zheng Chang <zhengchang@microsoft.com>
2021-04-20 17:51:29 +0800

    update

--- test/scripts/deprovision.sh
+++ test/scripts/deprovision.sh
@@ -1,1 +1,8 @@
-echo "#vmAdminPassword#" | sudo -S waagent -deprovision+user -force | exit
+#!/bin/bash
+function deprovision() {
+    echo "#vmAdminPassword#" | sudo -S waagent -deprovision+user -force
+    return 1
+}
+
+deprovision || { echo 'done'; exit 1}
+echo 'something went wrong'
